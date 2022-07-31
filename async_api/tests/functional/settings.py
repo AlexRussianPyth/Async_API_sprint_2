7a3d4900919e0c0636e31c054d80ccab0d02a444
Alex <alexvkleschov@gmail.com>
2022-07-31 11:16:43 +0400

    fix env vars in tests, removed python-dotenv

--- async_api/tests/functional/settings.py
+++ async_api/tests/functional/settings.py
@@ -1,15 +1,10 @@
-import os
-import pathlib
-
-from dotenv import load_dotenv
 from pydantic import BaseSettings, Field
 
 
-load_dotenv(os.path.join(pathlib.Path(__file__).parent.absolute(), '.env'))
-
-
 class TestSettings(BaseSettings):
-    es_host: str = Field('http://127.0.0.1:9200', env='ES_HOST')
+    es_host: str = Field('http://127.0.0.1', env='ES_HOST')
+    es_port: str = Field('9200', env='ES_PORT')
+
     redis_host: str = Field('127.0.0.1', env='REDIS_HOST')
     redis_port: str = Field('6379', env='REDIS_PORT')
 
