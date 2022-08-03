ba8443f68acd2bc27f4372500a5dfb31054c5278
denis <petrovsky.dv@gmail.com>
2022-08-03 13:35:47 +0300

    add cache test for test_person_by_id

--- async_api/tests/functional/src/test_person.py
+++ async_api/tests/functional/src/test_person.py
@@ -1,3 +1,4 @@
+import json
 import random
 from http import HTTPStatus
 
@@ -9,21 +10,33 @@ from async_api.tests.functional.testdata.persons_data import es_persons
 pytestmark = pytest.mark.asyncio
 
 
-async def test_person_by_id(es_client, make_get_request, persons_index):
+async def test_person_by_id(es_client, make_get_request, redis_client, persons_index):
     """Поиск конкретного человека"""
 
     es_person = random.choice(es_persons)
+    await redis_client.flushall()
+    assert await redis_client.get(key=es_person['id']) is None
+
     response = await make_get_request(endpoint=f"{test_settings.person_router_prefix}/{es_person['id']}")
     assert response.status == HTTPStatus.OK
     api_person = response.body
+    assert isinstance(api_person, dict)
+    assert es_person['id'] == api_person.get('uuid')
+    assert es_person['full_name'] == api_person.get('full_name')
+    assert es_person['role'] == api_person.get('role')
 
-    assert es_person['id'] == api_person['uuid']
-    assert es_person['full_name'] == api_person['full_name']
-    assert es_person['role'] == api_person['role']
+    cache_person_json = await redis_client.get(key=es_person['id'])
+    assert cache_person_json
+    cache_person = json.loads(cache_person_json)
+    assert isinstance(cache_person, dict)
+    assert es_person['id'] == cache_person.get('id')
+    assert es_person['full_name'] == cache_person.get('full_name')
+    assert es_person['role'] == cache_person.get('role')
 
 
 async def test_films_by_person(es_client, make_get_request, persons_index):
     """Поиск всех фильмов с участием человека"""
+
     es_person = random.choice(es_persons)
     response = await make_get_request(endpoint=f"{test_settings.person_router_prefix}/{es_person['id']}/film")
     assert response.status == HTTPStatus.OK
