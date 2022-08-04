829dfb0a6a9c51eac6394fef3d6efaf67c51af96
denis <petrovsky.dv@gmail.com>
2022-08-04 23:34:08 +0300

    add load movies data

--- async_api/tests/functional/conftest.py
+++ async_api/tests/functional/conftest.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from dataclasses import dataclass
 
 import aiohttp
@@ -84,5 +85,42 @@ async def persons_index(es_client):
     await es_client.indices.create(index=index_name, body=es_persons_index_schema, ignore=400)
     persons = [{"_index": index_name, "_id": obj.get("id"), **obj} for obj in es_persons]
     await async_bulk(client=es_client, actions=persons)
+    await asyncio.sleep(1)  # ждем чтобы индекс успел обновиться
+
     yield
+
+    await es_client.indices.delete(index=index_name)
+
+
+@pytest.fixture(scope='session')
+async def movies_index(es_client):
+    """Создание и заполнение индекса в Elastic"""
+
+    index_name = 'movies'
+    await es_client.indices.create(index=index_name, body=es_films_index_schema, ignore=400)
+    with open('../testdata/movies.json') as file:
+        es_films = json.load(file)
+    films = []
+    for es_film in es_films:
+        film = {
+            "_index": index_name,
+            "_id": es_film.get("id"),
+            "id": es_film.get("id"),
+            "title": es_film.get("title"),
+            "description": es_film.get("description"),
+            "imdb_rating": es_film.get("imdb_rating"),
+            "genre": es_film.get("genre"),
+            "actors": es_film.get("actors"),
+            "writers": es_film.get("writers"),
+            "actors_names": es_film.get("actors_names"),
+            "writers_names": es_film.get("writers_names"),
+            "director": es_film.get("director"),
+
+        }
+        films.append(film)
+    await async_bulk(client=es_client, actions=films)
+    await asyncio.sleep(1)  # ждем чтобы индекс успел обновиться
+
+    yield
+
     await es_client.indices.delete(index=index_name)
