99ecf20be26b75c1e504a224241fcd8167cb0e4f
Alex <alexvkleschov@gmail.com>
2022-08-06 13:42:57 +0400

    add test for testing search

--- async_api/tests/functional/settings.py
+++ async_api/tests/functional/settings.py
@@ -9,7 +9,7 @@ class TestSettings(BaseSettings):
     redis_port: str = Field('6379', env='REDIS_PORT')
 
     fastapi_host: str = Field('http://127.0.0.1', env='FASTAPI_HOST')
-    fastapi_port: str = Field('8002', env='FASTAPI_PORT')
+    fastapi_port: str = Field('8000', env='FASTAPI_PORT')
 
     person_router_prefix: str = Field('/api/v1/persons', env='PERSON_ROUTER_PREFIX')
     movies_router_prefix: str = Field('/api/v1/films', env='MOVIES_ROUTER_PREFIX')
--- async_api/tests/functional/src/test_search.py
+++ async_api/tests/functional/src/test_search.py
@@ -1,18 +1,48 @@
 import pytest
+from http import HTTPStatus
+from uuid import UUID
+from ..settings import test_settings
+from ..utils.cache import generate_cache_key
+
+from pydantic import BaseModel
 
 
-# TODO Доработать этот тест, он из примера в теории ЯП
-@pytest.mark.skip(reason='Not realised yet')
+class PersonShortInfo(BaseModel):
+    id: str
+    name: str
+
+class FilmSchema(BaseModel):
+    """Полный набор полей для эндпоинта с описанием одного фильма"""
+    uuid: UUID
+    title: str
+    imdb_rating: float
+    genre: list[str] | None
+    description: str | None
+    directors: list[str] | None
+    actors: list[PersonShortInfo] | None
+    writers: list[PersonShortInfo] | None
+
+# ПОИСК ПО ФИЛЬМАМ
 @pytest.mark.asyncio
-async def test_search_detailed(es_client, make_get_request):
-    # Заполнение данных для теста
-    await es_client.bulk(...)
+async def test_search_films(es_client, redis_client, make_get_request, movies_index):
+    """Проверяет работу поиска по фильмам"""
+    await redis_client.flushall()
+    # Выполняем поисковый запрос
+    response = await make_get_request(
+        endpoint='/api/v1/films/search',
+        params={'query': 'Disney'}
+    )
 
-    # Выполнение запроса
-    response = await make_get_request('/search', {'search': 'Star Wars'})
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) == 4
+    # Проверяем, что схемы верны
+    for film in response.body:
+        assert FilmSchema(**film) # TODO Так себе проверка?
 
-    # Проверка результата
-    assert response.status == 200
-    assert len(response.body) == 1
+    # проверяем кэширование
 
-    assert response.body == expected
+
+# ПОИСК ПО ПЕРССОНАМ
+
+
+
